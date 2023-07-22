191df3a596a907e1dd40edfea0d62194993c1369
yrstrulyjayanth <138367937+yrstrulyjayanth@users.noreply.github.com>
2023-07-22 20:09:05 +0530

    Add files via upload

---
+++ task.sql
@@ -0,0 +1,16 @@
+create database assignment;
+use assignment;
+create table movies (movie_id int primary key,title varchar(30),release_year int,director_id int);
+create table directors (director_id int primary key,director_name varchar(30));
+create table genres (genres_id int primary key,genre_name varchar(30));
+create table movgenres (movie_id int,genre_id int);
+insert into movies values (1,'bahubali',2015,1),(2,'ene',2018,2),(3,'polladhavan',2008,3);
+insert into movies values (4,'spy',2023,1),(5,'agent',2018,2),(6,'singh',2023,3);
+insert into directors values (1,'rajamouli'),(2,'sukumar'),(3,'vetrimaran');
+insert into genres values (1,'Action-Drama'),(2,'Action-Comedy');
+insert into movgenres values (1,1),(2,1),(3,2);
+insert into movgenres values (4,2),(5,1),(6,2);
+SELECT movies.title, directors.director_name FROM movies JOIN directors ON movies.director_id = directors.director_id;
+SELECT movies.title, movies.release_year, directors.director_name FROM movies LEFT JOIN directors ON movies.director_id = directors.director_id;
+SELECT directors.director_name, movies.title FROM directors LEFT JOIN movies ON directors.director_id = movies.director_id;
+SELECT movies.title, genres.genre_name FROM movies JOIN movgenres ON movies.movie_id = movgenres.movie_id JOIN genres ON movgenres.genre_id = genres.genres_id;
